4a7edbe978c1279b927024dfc05c59da88f38763
Qoo <Qoo@QooMac-mini.local>
2026-09-21 00:55:06 +0800

    feat: support Emby incremental media updates

--- src/api/emby.rs
+++ src/api/emby.rs
@@ -22,6 +22,7 @@ pub(super) fn api_routes() -> Router<AppState> {
             get(emby_library_selectable_media_folders),
         )
         .route("/Library/MediaFolders", get(emby_library_media_folders))
+        .route("/Library/Media/Updated", post(emby_media_updated))
         .route("/Persons", get(emby_persons))
         .route("/Persons/{person_id}", get(emby_person))
         .route(
